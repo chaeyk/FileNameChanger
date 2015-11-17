f0a4a337f00f7281ce8a778ecc6e7427dcab7e4b
chaeyk <chaeyk@gmail.com>
2015-11-18 01:18:20 +0900

    First commit

---
+++ FileNameChanger/FrmMain.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace FileNameChanger
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDate2Name = new System.Windows.Forms.Button();
+            this.btnName2Date = new System.Windows.Forms.Button();
+            this.lvFiles = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRevert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAdd.Location = new System.Drawing.Point(12, 240);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDelete.Location = new System.Drawing.Point(93, 240);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClear.Location = new System.Drawing.Point(174, 240);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDate2Name
+            // 
+            this.btnDate2Name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDate2Name.Location = new System.Drawing.Point(414, 240);
+            this.btnDate2Name.Name = "btnDate2Name";
+            this.btnDate2Name.Size = new System.Drawing.Size(155, 23);
+            this.btnDate2Name.TabIndex = 5;
+            this.btnDate2Name.Text = "Date -> Filename";
+            this.btnDate2Name.UseVisualStyleBackColor = true;
+            this.btnDate2Name.Click += new System.EventHandler(this.btnDate2Name_Click);
+            // 
+            // btnName2Date
+            // 
+            this.btnName2Date.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnName2Date.Location = new System.Drawing.Point(576, 240);
+            this.btnName2Date.Name = "btnName2Date";
+            this.btnName2Date.Size = new System.Drawing.Size(152, 23);
+            this.btnName2Date.TabIndex = 6;
+            this.btnName2Date.Text = "Filename -> Date";
+            this.btnName2Date.UseVisualStyleBackColor = true;
+            this.btnName2Date.Click += new System.EventHandler(this.btnName2Date_Click);
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvFiles.FullRowSelect = true;
+            this.lvFiles.Location = new System.Drawing.Point(12, 13);
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(716, 221);
+            this.lvFiles.TabIndex = 0;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            this.lvFiles.View = System.Windows.Forms.View.Details;
+            this.lvFiles.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lvFiles_KeyDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "FileName";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Path";
+            this.columnHeader3.Width = 250;
+            // 
+            // tbLog
+            // 
+            this.tbLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLog.BackColor = System.Drawing.SystemColors.Window;
+            this.tbLog.Location = new System.Drawing.Point(12, 269);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.Size = new System.Drawing.Size(716, 119);
+            this.tbLog.TabIndex = 7;
+            this.tbLog.TabStop = false;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Original";
+            this.columnHeader4.Width = 150;
+            // 
+            // btnRevert
+            // 
+            this.btnRevert.Location = new System.Drawing.Point(256, 240);
+            this.btnRevert.Name = "btnRevert";
+            this.btnRevert.Size = new System.Drawing.Size(75, 23);
+            this.btnRevert.TabIndex = 4;
+            this.btnRevert.Text = "Revert";
+            this.btnRevert.UseVisualStyleBackColor = true;
+            this.btnRevert.Click += new System.EventHandler(this.btnRevert_Click);
+            // 
+            // FrmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 400);
+            this.Controls.Add(this.btnRevert);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.lvFiles);
+            this.Controls.Add(this.btnName2Date);
+            this.Controls.Add(this.btnDate2Name);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "FrmMain";
+            this.Text = "File Name Changer";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FrmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FrmMain_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDate2Name;
+        private System.Windows.Forms.Button btnName2Date;
+        private System.Windows.Forms.ListView lvFiles;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btnRevert;
+    }
+}
+
---
+++ FileNameChanger/FrmMain.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace FileNameChanger
+{
+    public partial class FrmMain : Form
+    {
+        public class FileData
+        {
+            public string filename;
+
+            public string originalFilename;
+            public DateTime originalCDate;
+            public DateTime originalMDate;
+
+            public FileData(string filename)
+            {
+                this.filename = filename;
+
+                originalFilename = filename;
+                originalCDate = File.GetCreationTime(filename);
+                originalMDate = File.GetLastWriteTime(filename);
+            }
+
+            public bool Equals(object obj)
+            {
+                return filename.Equals(obj);
+            }
+
+            public void SyncTo(ListViewItem item)
+            {
+                string path = Path.GetDirectoryName(filename);
+                string name = Path.GetFileName(filename);
+                DateTime date = DateFromFile(filename);
+
+                while (item.SubItems.Count < 4)
+                    item.SubItems.Add("");
+
+                item.SubItems[0].Text = name;
+                item.SubItems[1].Text = date.ToString();
+                item.SubItems[2].Text = path;
+                item.SubItems[3].Text = Path.GetFileName(originalFilename);
+            }
+        }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private static DateTime DateFromFilename(string filename)
+        {
+            string name = Path.GetFileNameWithoutExtension(filename);
+            if (name.Length > 14)
+                name = name.Substring(0, 14);
+
+            return DateTime.ParseExact(name, "yyyyMMddHHmmss", CultureInfo.InvariantCulture);
+        }
+
+        private static DateTime DateFromFile(string filename)
+        {
+            DateTime date = File.GetCreationTime(filename);
+
+            if (date.Year < 1900)
+                throw new Exception("Failed to get creation time of " + filename);
+
+            return date;
+        }
+
+        private void FrmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void FrmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string filename in files)
+            {
+                AddFile(filename);
+            }
+        }
+
+        private void AddFile(string filename)
+        {
+            DateTime date = DateFromFile(filename);
+            try
+            {
+                DateTime dateFromName = DateFromFilename(filename);
+                if (date.Equals(dateFromName))
+                {
+                    WriteLog(filename + " has same creation datetime. ignored");
+                    return;
+                }
+            }
+            catch (FormatException)
+            {
+                // ignore
+            }
+
+            foreach (ListViewItem i in lvFiles.Items)
+            {
+                if (i.Tag.Equals(filename))
+                    return;
+            }
+
+            FileData fileData = new FileData(filename);
+            ListViewItem item = new ListViewItem();
+            item.Tag = fileData;
+            fileData.SyncTo(item);
+
+            lvFiles.Items.Add(item);
+        }
+
+        private void DeleteFile()
+        {
+            if (lvFiles.SelectedItems.Count <= 0)
+                return;
+
+            ListViewItem[] items = new ListViewItem[lvFiles.SelectedItems.Count];
+            lvFiles.SelectedItems.CopyTo(items, 0);
+            foreach (ListViewItem item in items)
+            {
+                lvFiles.Items.Remove(item);
+            }
+        }
+
+        private void ClearFile()
+        {
+            lvFiles.Items.Clear();
+        }
+
+        private void RevertFile(ListViewItem item)
+        {
+            FileData fileData = (FileData)item.Tag;
+
+            File.SetCreationTime(fileData.filename, fileData.originalCDate);
+            File.SetLastWriteTime(fileData.filename, fileData.originalMDate);
+            File.Move(fileData.filename, fileData.originalFilename);
+
+            fileData.filename = fileData.originalFilename;
+            fileData.SyncTo(item);
+        }
+
+        private void WriteLog(string msg)
+        {
+            tbLog.AppendText(msg + "\r\n");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string filename in dlg.FileNames)
+                {
+                    AddFile(filename);
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DeleteFile();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFile();
+        }
+
+        private void lvFiles_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                DeleteFile();
+            }
+        }
+
+        private void DateToFilename(ListViewItem item)
+        {
+            FileData fileData = (FileData)item.Tag;
+            string filename = fileData.filename;
+            DateTime date = DateFromFile(filename);
+
+            string path = Path.GetDirectoryName(filename);
+            string ext = Path.GetExtension(filename);
+            string name = date.ToString("yyyyMMddHHmmss");
+
+            string newFilename = path + "\\" + name + ext;
+
+            if (!filename.Equals(newFilename))
+            {
+                try
+                {
+                    File.Move(filename, newFilename);
+
+                    fileData.filename = newFilename;
+                    fileData.SyncTo(item);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("File rename failed [" + filename + " -> " + newFilename + "]", e);
+                }
+            }
+        }
+
+        private void btnDate2Name_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in lvFiles.Items)
+            {
+                try
+                {
+                    DateToFilename(item);
+                }
+                catch (Exception ex)
+                {
+                    WriteLog(ex.Message);
+                }
+            }
+        }
+
+        private void FilenameToDate(ListViewItem item)
+        {
+            FileData fileData = (FileData)item.Tag;
+            string filename = fileData.filename;
+            try
+            {
+                DateTime date = DateFromFilename(filename);
+
+                File.SetCreationTime(filename, date);
+                File.SetLastWriteTime(filename, date);
+
+                fileData.SyncTo(item);
+            }
+            catch (FormatException e)
+            {
+                throw new Exception(filename + " doesn't have date information", e);
+            }
+        }
+
+        private void btnName2Date_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in lvFiles.Items)
+            {
+                try
+                {
+                    FilenameToDate(item);
+                }
+                catch (Exception ex)
+                {
+                    WriteLog(ex.Message);
+                }
+            }
+        }
+
+        private void btnRevert_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in lvFiles.Items)
+            {
+                try
+                {
+                    RevertFile(item);
+                }
+                catch (Exception ex)
+                {
+                    WriteLog(ex.Message);
+                }
+            }
+        }
+    }
+}
